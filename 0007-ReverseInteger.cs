a32bffc00282b8cbaa8c4dd58a5ae97da22a9403
Ivan Zelenin <zeleninivans@gmail.com>
2021-12-27 20:29:15 +0300

    Add 7th task

---
+++ 0007-ReverseInteger.cs
@@ -0,0 +1,25 @@
+﻿namespace Task7
+{
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+            long result = 0;
+            int sign = Math.Sign(x);
+            x *= sign;
+
+            while (x > 0)
+            {
+                result = result * 10 + x % 10;
+                x /= 10;
+            }
+            result *= sign;
+
+            if (result < -(1L << 31))
+                return 0;
+            if (result >= (1L << 31))
+                return 0;
+            return (int)result;
+        }
+    }
+}
