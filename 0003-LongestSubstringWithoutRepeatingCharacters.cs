323899fed47beff677278f03f1f4507d8ae8a174
Ivan Zelenin <zeleninivans@gmail.com>
2021-12-27 22:58:41 +0300

    Add 3rd task

---
+++ 0003-LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,26 @@
+﻿namespace Task3
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            int result = 0;
+
+            HashSet<char> set = new HashSet<char>();
+            int j = 0;
+            for (int i = 0; i < s.Length; ++i)
+            {
+                while(j < s.Length && !set.Contains(s[j]))
+                {
+                    set.Add(s[j]);
+                    ++j;
+                }
+
+                result = Math.Max(result, j - i);
+                set.Remove(s[i]);
+            }
+
+            return result;
+        }
+    }
+}
