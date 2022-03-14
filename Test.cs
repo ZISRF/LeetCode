afacdb79ee9735324dcd31da3f923ce17d0ce888
Zelenin Ivan <zeleninivans@gmail.com>
2022-03-14 16:23:26 +0300

    delete Test.cs

--- Test.cs
+++ Test.cs
@@ -7,7 +7,6 @@ namespace Setup
         static void Main(string[] args)
         {
             var sln = new Task4.Solution();
-
             Console.WriteLine(sln.FindMedianSortedArrays(new int[] { 1, 3 }, new int[] { 2, 4 }));
         }
     }
